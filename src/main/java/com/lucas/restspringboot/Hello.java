14034c2f1c5eee5b8fcddbdba84714fcea425215
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2023-03-14 09:49:24 -0300

    First steps in java with spring boot

---
+++ src/main/java/com/lucas/restspringboot/Hello.java
@@ -0,0 +1,22 @@
+package com.lucas.restspringboot;
+
+public class Hello {
+
+    private final Long id;
+
+    private final String content;
+
+    public Hello(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
